067e7e02e80652b7677f4c7fd4ac1d90d56e947b
wu-linghui <1540996580@qq.com>
2021-06-09 00:23:48 +0800

    ts-axios

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { AxiosRequestConfig } from "./types";
 import xhr from "./xhr";
+import { bulidURL } from './helpers/url';
+
 function axios(config: AxiosRequestConfig):void {
     xhr(config);
+}
+
+function processConfig (config: AxiosRequestConfig): void {
+    config.url = transformURL(config);
+}
+
+function transformURL (config: AxiosRequestConfig): string {
+    const {url, params} = config;
+    return bulidURL(url, params)
 }
 
 export default axios;
